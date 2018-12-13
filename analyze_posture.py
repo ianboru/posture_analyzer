efb8b54cba0afc13c2873742c86bbd3505202e5e
ian b <ianboru@gmail.com>
2018-12-12 22:25:37 -0500

    modding for eye tracking data collection

--- analyze_posture.py
+++ analyze_posture.py
@@ -2,135 +2,82 @@ import numpy as np
 import cv2
 import math
 import time
-import os
 
-# Set resolution for the video capture
-# Function adapted from https://kirr.co/0l6qmh
-def change_res(cap, width, height):
-    cap.set(3, width)
-    cap.set(4, height)
+bg = cv2.imread('/Users/ianborukhovich/Projects/posture_analyzer/bg_lit.jpg')
+fg = cv2.imread('/Users/ianborukhovich/Projects/posture_analyzer/fg_lit.jpg')
+just_fg = cv2.subtract(fg,bg)
+imgray = cv2.cvtColor(just_fg, cv2.COLOR_BGR2GRAY)
 
-# Standard Video Dimensions Sizes
-# grab resolution dimensions and set video capture to it.
-def get_dims(cap, res='1080p'):
-    width, height = STD_DIMENSIONS["480p"]
-    if res in STD_DIMENSIONS:
-        width,height = STD_DIMENSIONS[res]
-    ## change the current caputre device
-    ## to the resulting resolution
-    change_res(cap, width, height)
-    return width, height
-
-def get_video_type(filename):
-    filename, ext = os.path.splitext(filename)
-    if ext in VIDEO_TYPE:
-      return  VIDEO_TYPE[ext]
-    return VIDEO_TYPE['avi']
-
-def draw_filtered_contours(image, contours):
-	mask = np.zeros(image.shape, np.uint8)
-	print("drawing contours")
-	for contour in contours:
-		if cv2.contourArea(contour) > 500:
-			cv2.drawContours(mask, [contour], -1, (255,0,0), -1)
-	return mask
-
-
+window_names = ["current","diffrev","assymetries"]
+window_size = 425
+#window_locations = [[0,0],[0,window_size],[window_size,0],[window_size,window_size],[window_size*2,window_size],[window_size*2,0]]
+window_locations = [[0,0],[window_size,0],[window_size*2,0]]
 def setup_windows():
-
-	window_names = ["current","diffrev","assymetries"]
-	window_size = 425
-	#window_locations = [[0,0],[0,window_size],[window_size,0],[window_size,window_size],[window_size*2,window_size],[window_size*2,0]]
-	window_sizes = [300,300,600]
-	window_locations = [[0,0],[0,300],[300,0]]
-
 	for i in range(0,len(window_names)):
 		cur_name = window_names[i]
 		cv2.namedWindow(cur_name,cv2.WINDOW_NORMAL)
-		cv2.resizeWindow(cur_name, window_sizes[i],window_sizes[i])
+		cv2.resizeWindow(cur_name, window_size,window_size)
 		cv2.moveWindow(cur_name, window_locations[i][0],window_locations[i][1])
-
-def convert_to_gray(image, threshold):
-#Find contours
-	imgray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-	ret, thresh = cv2.threshold(imgray, threshold, 255 ,cv2.THRESH_BINARY)
-	return thresh
-
-# Video Encoding, might require additional installs
-# Types of Codes: http://www.fourcc.org/codecs.php
-VIDEO_TYPE = {
-    'avi': cv2.VideoWriter_fourcc(*'XVID'),
-    #'mp4': cv2.VideoWriter_fourcc(*'H264'),
-    'mp4': cv2.VideoWriter_fourcc(*'XVID'),
-}
-
-filename = 'video.avi'
-frames_per_second = 24.0
-res = '720p'
-STD_DIMENSIONS =  {
-    "480p": (640, 480),
-    "720p": (1280, 720),
-    "1080p": (1920, 1080),
-    "4k": (3840, 2160),
-}
-
-cam = cv2.VideoCapture(0)
-out = cv2.VideoWriter(filename, get_video_type(filename), 25, get_dims(cam, res))
-width = cam.get(cv2.CAP_PROP_FRAME_WIDTH)
-height = cam.get(cv2.CAP_PROP_FRAME_HEIGHT)
-shape = (int(width), int(height))
-
+	
 img_counter = 0
 space_counter = 0
 timing = False
 start = time.time()
 font = cv2.FONT_HERSHEY_SIMPLEX
 snapshot_timer_length = 4
+cam = cv2.VideoCapture(0)
 setup_windows()
 center_line_width = 20
-grayscale_filter_threshold = 127
-
-def initialize_frames():
-	bg = np.zeros(shape, np.uint8)
-	fg = np.zeros(shape, np.uint8)
-	diff = np.zeros(shape, np.uint8)
-	mask = np.zeros(shape, np.uint8)
-	assymetries = np.zeros(shape, np.uint8)
-	text_mask = np.zeros(shape, np.uint8)
-	diffrev = np.zeros(shape, np.uint8)
-	gray = np.zeros(shape, np.uint8)
-	return bg,fg,diff,mask,assymetries,text_mask,diffrev,gray
-
-bg,fg,diff,mask,assymetries,text_mask,diffrev,gray = initialize_frames()
-cv2.putText(text_mask, 'Take BG snap',(10,500), font, 4,(255,255,255),2,cv2.LINE_AA)
 while True:
 	ret, frame = cam.read()
+	width = cam.get(cv2.CAP_PROP_FRAME_WIDTH)
+	height = cam.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
 	if not ret:
 		break
 	frame = cv2.flip(frame,1)
 	print("frame " + str(img_counter))
 
-		
-	cv2.putText(text_mask, 'Thresh = ' + str(grayscale_filter_threshold),(10,200), font, 4,(255,255,255),2,cv2.LINE_AA)
-	
-	mask = draw_filtered_contours(gray_diff, contours)
+	if img_counter == 0:
+		bg = np.zeros(frame.shape, np.uint8)
+		fg = np.zeros(frame.shape, np.uint8)
+		diff = np.zeros(frame.shape, np.uint8)
+		mask = np.zeros(frame.shape, np.uint8)
+		assymetries = np.zeros(frame.shape, np.uint8)
+		text_mask = np.zeros(frame.shape, np.uint8)
+		cv2.putText(text_mask, 'Take BG snap',(10,500), font, 4,(255,255,255),2,cv2.LINE_AA)
+	#Find contours
+	imgray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+	ret, thresh = cv2.threshold(imgray, 127, 255 ,10)
+	im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
 	if len(contours) > 0:
+		mask = np.zeros(im2.shape, np.uint8)
+		print("drawing contours")
+
+		for contour in contours:
+			if cv2.contourArea(contour) > 50:
+				cv2.drawContours(mask, [contour], -1, (255,0,0), -1)
+
 		mask_reflected = mask.copy()
 		mask_reflected = cv2.flip( mask_reflected, 1 )
 		assymetries = cv2.subtract(mask,mask_reflected)
 	if timing:
 		time_diff = str(math.floor(time.time()-start))
-		text_mask = np.zeros(shape, np.uint8)
+		text_mask = np.zeros(frame.shape, np.uint8)
 		cv2.putText(text_mask, time_diff + ' seconds BG snap',(10,500), font, 4,(255,255,255),2,cv2.LINE_AA)
+	
 	display_frame = frame.copy()
 	cv2.rectangle(display_frame, (math.floor(width/2), 0), (math.floor(width/2), int(height)), (255,0,0), center_line_width)
 	cv2.imshow("current", cv2.add(display_frame,text_mask))
+	cv2.imshow("diffrev", mask)
 	if np.any(mask):
+		#frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		#frame_ret, frame_thresh = cv2.threshold(frame_gray, 127, 255 ,10)
 		assymetries = cv2.cvtColor(assymetries,cv2.COLOR_GRAY2RGB)
 		assymetries =  cv2.flip( assymetries, 1 )
+
 		assymetries = cv2.add(frame,assymetries)
 		cv2.rectangle(assymetries, (math.floor(width/2), 0), (math.floor(width/2), int(height)), (255,0,0), center_line_width)
-		cv2.imshow("diffrev", mask)
 		cv2.imshow("assymetries", assymetries )
 
 	k = cv2.waitKey(1)
@@ -141,23 +88,20 @@ while True:
 		start = time.time()
 		timing = True
 		if space_counter == 1:
-			bg,fg,diff,mask,assymetries,text_mask,diffrev,gray = initialize_frames()
+			bg = np.zeros(frame.shape, np.uint8)
+			fg = np.zeros(frame.shape, np.uint8)
+			diff = np.zeros(frame.shape, np.uint8)
+			diffrev = np.zeros(frame.shape, np.uint8)
+			gray = np.zeros(frame.shape, np.uint8)
 			start = time.time()
-	elif k%256 == ord('u'):
-		grayscale_filter_threshold += 1
-	elif k%256 == ord('y'):	
-		grayscale_filter_threshold -= 1
 
 	print(time.time()-start)
 	if np.any(bg):
 		fg = frame.copy()
-		diffrev = cv2.subtract(fg,bg)
-		text_mask = np.zeros(shape, np.uint8)
+		diff = cv2.subtract(fg,bg)
+		diffrev = cv2.subtract(bg,fg)
+		text_mask = np.zeros(frame.shape, np.uint8)
 		cv2.putText(text_mask, 'Retake BG photo',(10,500), font, 4,(255,255,255),2,cv2.LINE_AA)	
-		gray_fg = convert_to_gray(fg,grayscale_filter_threshold)
-		gray_bg = convert_to_gray(bg,grayscale_filter_threshold)
-		gray_diff = cv2.subtract(gray_fg,gray_bg)
-		im2, contours, hierarchy = cv2.findContours(gray_diff, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
 	if timing and time.time()-start > snapshot_timer_length :
 		timing = False
 		start = time.time()
@@ -166,5 +110,5 @@ while True:
 					
 	img_counter += 1
 
-out.release()
+cv2.waitKey(0)
 cv2.destroyAllWindows()
--- posture_perfect/src/App.js
+++ posture_perfect/src/App.js
@@ -5,6 +5,8 @@ import cv from 'opencv.js';
 import * as tf from '@tensorflow/tfjs';
 
 import * as posenet from '@tensorflow-models/posenet';
+let scrollX = null;
+let scrollY = null;
 class App extends Component {
 
   state = {
@@ -20,23 +22,35 @@ class App extends Component {
     srcMat : null,
     net : null,
     startTime : Date.now(),
-    backgroundTimer: 4,
-    background : null,
-    lh:5,
-    ls:5,
-    lv:5,
-    hh:30,
-    hs:30,
-    hv:30,
-    upsideDownMode:false,
+    lastEyeHeight : null,
+    scrollX : null,
+    scrollY : null,
   }
   componentDidMount=()=>{
+    const targetSize = 20
+
+    if ('scrollRestoration' in window.history) {
+      window.history.scrollRestoration = 'manual'
+    }
     posenet.load().then(data=>{
        this.setState({
           net : data
         })
 
     });
+    console.log("mounted")
+    setInterval(()=>{
+      console.log("next loop")
+      const y = Math.floor(Math.random() * window.innerHeight)
+
+      const x =  Math.floor(Math.random() * window.innerWidth)
+      console.log("x,y" , x,y)
+      let domElement = document.getElementById("lookHere")
+      domElement.style.position = "absolute";
+      domElement.style.top = y-targetSize + "px"; //or whatever 
+      domElement.style.left = x-targetSize + "px";
+      console.log(domElement)
+    }, 2000)
   }
   startCamera=()=> {
     let that = this
@@ -74,14 +88,19 @@ class App extends Component {
     }, false);
   }
   startVideoProcessing=()=> {
-    var vidLength = 30 //seconds
-    var fps = 24;
+
     if (!this.state.streaming) { console.warn("Please startup your webcam"); return; }
+    
     this.stopVideoProcessing();
+    let canvasOutputCtx = this.canvasOutput.getContext('2d');
+    this.setState({
+      canvasOutputCtx
+    })
     let srcMat = new cv.Mat(this.state.videoHeight, this.state.videoWidth, cv.CV_8UC4);
     this.setState({
       srcMat
     })
+
     requestAnimationFrame(this.processVideo);
   }
 
@@ -105,19 +124,20 @@ class App extends Component {
     return 'hsl('+c+', 100%, 50%)';
   }
   connectConsecutiveParts=(context, keypointPositions, parts)=>{
-    ["left","right"].forEach((side)=>{
-      parts.forEach((part, i)=>{
-        if(i < parts.length){
-          context.beginPath()
-          if(keypointPositions[side+parts[i]] && keypointPositions[side+parts[i+1]]){
-              context.moveTo(keypointPositions[side+parts[i]].x,keypointPositions[side+parts[i]].y)
-              context.lineTo(keypointPositions[side+parts[i+1]].x,keypointPositions[side+parts[i+1]].y)
-          }
+    parts.forEach((part, i)=>{
+      if(i < parts.length){
+        context.beginPath()
+        if(keypointPositions["left"+parts[i]] && keypointPositions["left"+parts[i+1]]){
+          context.moveTo(keypointPositions["left"+parts[i]].x,keypointPositions["left"+parts[i]].y)
+          context.lineTo(keypointPositions["left"+parts[i+1]].x,keypointPositions["left"+parts[i+1]].y)
         }
-        context.stroke(); 
-      })
+        if(keypointPositions["right"+parts[i]] && keypointPositions["right"+parts[i+1]]){
+          context.moveTo(keypointPositions["right"+parts[i]].x,keypointPositions["right"+parts[i]].y)
+          context.lineTo(keypointPositions["right"+parts[i+1]].x,keypointPositions["right"+parts[i+1]].y)
+        }
+      }
+      context.stroke(); 
     })
-    
     
 
   }
@@ -144,13 +164,12 @@ class App extends Component {
         context.stroke(); 
         context.font="25px Verdana"
         context.fillStyle = this.hslColPercent(anglePercent,0,120);
-        //context.fillText(angle.toFixed(1) + String.fromCharCode(176),xFrom+15,Math.floor((yFrom+yTo)/2));
+
+        context.fillText(angle.toFixed(1) + String.fromCharCode(176),xFrom+15,Math.floor((yFrom+yTo)/2));
 
       }
     })
     let parts = ["Shoulder","Elbow", "Wrist"]
-    this.connectConsecutiveParts(context, keypointPositions, parts)
-    parts = ["Hip","Knee", "Ankle"]
     this.connectConsecutiveParts(context, keypointPositions, parts)
     if(
         keypointPositions["leftShoulder"] && 
@@ -199,192 +218,69 @@ class App extends Component {
       context.stroke(); 
     }
   }
-  
-  captureBackground =()=>{
-  	let videoWidth = this.state.videoWidth
-    let videoHeight = this.state.videoHeight
-  	let canvasOutputCtx = this.canvasOutput.getContext("2d")
-
- 
-    let currentBackground = new cv.Mat(videoHeight, videoWidth, cv.CV_8UC4);
-    
-  	let interval = setInterval(()=>{
-  		this.setState({
-	  		backgroundTimer : this.state.backgroundTimer - 1,
-	  	},()=>{
-	  		if(this.state.backgroundTimer <= 0){
-		  		canvasOutputCtx.drawImage(this.video, 0, 0, videoWidth, videoHeight);
-		    	let imageData = canvasOutputCtx.getImageData(0, 0, videoWidth, videoHeight);
-		  		currentBackground.data.set(imageData.data);
-	    		cv.flip(currentBackground, currentBackground,1)
-
-		  		this.setState({
-		  			backgroundTimer : 4,
-		  			background : currentBackground.clone(),
-		  		},()=>{
-		  			clearInterval(interval)
-		  		})	
-	  		}
-	  	})
-  		
-  		
-  	},1000)
-  }
-  drawFilteredContours=(src)=>{
-
-    let dst = cv.Mat.zeros(this.state.videoHeight, this.state.videoWidth, cv.CV_8UC4);
-    cv.cvtColor(src, src, cv.COLOR_RGBA2GRAY, 0);
-    cv.threshold(src, src, 100, 255, cv.THRESH_BINARY_INV );//+ cv.THRESH_OTSU
-  
-    let contours = new cv.MatVector();
-    let hierarchy = new cv.Mat();
-    cv.findContours(src, contours, hierarchy, cv.RETR_LIST, cv.CHAIN_APPROX_NONE);
-    for (let i = 0; i < contours.size(); ++i) {
-        let color = new cv.Scalar(150,0,0)
-        cv.drawContours(dst, contours, i, color, 4, cv.LINE_8, hierarchy, 200);
-    }
-    //src.delete(); dst.delete(); contours.delete(); hierarchy.delete();
-
-    return dst;
-
-  }
-  addCenterLine = (context)=>{
-    context.lineWidth = 6;
-    context.strokeStyle = 'rgba(255,255,255,0.7)';
-    context.strokeRect(Math.floor(this.state.videoWidth/2)-10, 0, 20, this.state.videoHeight);
-    
-  }
-  filterOverlappingKeypoints=(keypointPositions, keypointScores)=>{
-    let filteredKeypoints = {}
-    for(let curKeypoint in keypointPositions){
-      let curPosition = keypointPositions[curKeypoint]
-      for(let otherKeypoint in keypointPositions){
-        let otherPosition = keypointPositions[otherKeypoint]
-        if(curKeypoint != otherKeypoint){
-          const distance = 
-          Math.pow(
-            Math.pow(curPosition.x - otherPosition.x,2) + 
-            Math.pow(curPosition.y - otherPosition.y,2) 
-          ,.5)
-          if(curKeypoint == "leftWrist" && otherKeypoint == "rightWrist"){
-            console.log(distance)
-
-          }
-          if(distance < 30 && filteredKeypoints[curKeypoint]){
-            delete filteredKeypoints[curKeypoint]
-            continue
-          }else{
-            filteredKeypoints[curKeypoint] = keypointPositions[curKeypoint]
-          }
-        } 
-      }
-    }
-    return filteredKeypoints
-  }
-  addPoseData = (context,poseData)=>{
-    context.lineWidth = 4;
-    context.strokeStyle = 'rgba(0,0,255,0.6)'
-    const scoreThreshold = .15
-    let keypointPositions = {}
-    let keypointScores = {}
-    if(this.state.pose){
-      let boxSize = 20
-      this.state.pose.keypoints.forEach((keypoint)=>{
-        if(keypoint.score > scoreThreshold && keypoint.part.indexOf("nose") == -1 && keypoint.part.indexOf("Ear") == -1){
-          if(this.state.upsideDownMode){
-            keypoint.position.x = keypoint.position.x - 2*(keypoint.position.x -this.state.videoWidth/2)
-
-          }
-          keypointPositions[keypoint.part] = keypoint.position
-          keypointScores[keypoint.part] = keypoint.score
-          context.strokeRect(keypoint.position.x - boxSize/2 , keypoint.position.y - boxSize/2, boxSize, boxSize);
-        }
-      })
-      //keypointPositions = this.filterOverlappingKeypoints(keypointPositions, keypointScores)
-      this.drawCrossBodyLines(context, keypointPositions)
-    }
-  }
   processVideo=()=> {
     let canvasOutputCtx = this.canvasOutput.getContext("2d")
     let videoWidth = this.state.videoWidth
     let videoHeight = this.state.videoHeight
-    let reflectedMat 
-    canvasOutputCtx.drawImage(this.video, 0, 0, videoWidth, videoHeight);
-    let imageData = canvasOutputCtx.getImageData(0, 0, videoWidth, videoHeight);
-    this.state.srcMat.data.set(imageData.data);
-    cv.flip(this.state.srcMat, this.state.srcMat,1)
-
-    if(this.state.upsideDownMode){  
-      reflectedMat = new cv.Mat(videoHeight, videoWidth, cv.CV_8UC4);
-      cv.flip(this.state.srcMat, reflectedMat,0)
-      cv.imshow('canvasOutput',reflectedMat)
-    }
-    let poseData = canvasOutputCtx.getImageData(0, 0, videoWidth, videoHeight);
     if(this.state.net){
-      var imageScaleFactor = .5;
-      var outputStride = 32;
+      var imageScaleFactor = 0.5;
+      var outputStride = 16;
       var flipHorizontal = true;
-        this.state.net.estimateSinglePose(poseData,imageScaleFactor, flipHorizontal, outputStride).then(pose=>{
+        this.state.net.estimateSinglePose(this.video,imageScaleFactor, flipHorizontal, outputStride).then(pose=>{
           this.setState({
             pose
           })
         });
     }
-    /*
-    let dst = new cv.Mat();
-    let low = new cv.Mat(this.state.srcMat.rows, this.state.srcMat.cols, this.state.srcMat.type(), [this.state.lh, this.state.ls, this.state.lv, 0]);
-    let high = new cv.Mat(this.state.srcMat.rows, this.state.srcMat.cols, this.state.srcMat.type(), [this.state.hh, this.state.hs, this.state.hv, 255]);
-    // You can try more different parameters
-    cv.inRange(this.state.srcMat, low, high, dst);
-    let M = cv.Mat.ones(2, 2, cv.CV_8U);
-    cv.erode(dst, dst, M);
-    cv.dilate(dst, dst, M);
-    cv.dilate(dst, dst, M, new cv.Point(-1, -1), 3);
     
-    cv.imshow('canvasOutput',dst)
-    low.delete()
-    high.delete()*/
-    if(this.state.background){
-    	let foregroundMat = new cv.Mat(videoHeight, videoWidth, cv.CV_8UC4);
-    	let drawingFlipMat = new cv.Mat(videoHeight, videoWidth, cv.CV_8UC4);
-    	let finalMat = new cv.Mat(videoHeight, videoWidth, cv.CV_8UC4);
+    canvasOutputCtx.drawImage(this.video, 0, 0, videoWidth, videoHeight);
+    let imageData = canvasOutputCtx.getImageData(0, 0, videoWidth, videoHeight);
+    this.state.srcMat.data.set(imageData.data);
+    cv.flip(this.state.srcMat, this.state.srcMat,1)
+    cv.imshow("canvasOutput", this.state.srcMat);
 
-    	cv.subtract(this.state.background,this.state.srcMat , foregroundMat)
+    canvasOutputCtx.lineWidth = 4;
+    canvasOutputCtx.strokeStyle = 'rgba(0,0,255,0.6)'
+    const scoreThreshold = .35
+    let keypointPositions = {}
+    if(this.state.pose){
+      let boxSize = 20
+      this.state.pose.keypoints.forEach((keypoint,index)=>{
+        if(keypoint.score > scoreThreshold && keypoint.part.indexOf("nose") == -1 && keypoint.part.indexOf("Ear") == -1){
+          keypointPositions[keypoint.part] = keypoint.position
+          canvasOutputCtx.strokeRect(keypoint.position.x - boxSize/2 , keypoint.position.y - boxSize/2, boxSize, boxSize);
+        }
 
-    	let drawing = this.drawFilteredContours(this.state.srcMat.clone())
-	    cv.flip(drawing, drawingFlipMat,1)
-	    cv.subtract(drawingFlipMat,drawing, drawingFlipMat)
-	    cv.add(this.state.srcMat,foregroundMat,finalMat)
-    	cv.imshow('canvasOutput',finalMat)
-    	foregroundMat.delete()
-    	drawing.delete()
-    	drawingFlipMat.delete()
-      finalMat.delete()
-    }else{
-      if(!this.state.upsideDownMode){
-        cv.imshow('canvasOutput',this.state.srcMat)
+        if(keypoint.part == "leftEye"){
+          let currentEyeHeight = keypoint.position.y
+           if(!this.state.lastEyeHeight){
+              this.setState({
+                  lastEyeHeight : currentEyeHeight
+                })
+            }
+          const heightDiff = currentEyeHeight - this.state.lastEyeHeight
+          if(heightDiff > 0){
+            window.scrollBy(0, heightDiff*4 )
+          }else{
+            window.scrollBy(0, heightDiff*6 )
+          }
+           
+           this.drawCrossBodyLines(canvasOutputCtx, keypointPositions)
+           console.log("left eye", keypoint.position.x, keypoint.position.y )
+        }
         
-      }
-    }
-    
- 	  //this.addCenterLine(canvasOutputCtx)
-    this.addPoseData(canvasOutputCtx, this.state.pose)
-    
-    this.drawGrid(canvasOutputCtx)
-    if(this.state.upsideDownMode){
-      imageData = canvasOutputCtx.getImageData(0, 0, videoWidth, videoHeight);
-      reflectedMat.data.set(imageData.data);
-      cv.flip(reflectedMat, reflectedMat,0)
-      cv.imshow('canvasOutput',reflectedMat)
-      reflectedMat.delete()
+      })
 
     }
+    this.drawGrid(canvasOutputCtx)
+
     var vidLength = 30 //seconds
     var fps = 24;
     var interval = 1000/fps;
     const delta = Date.now() - this.state.startTime;
-    
-
+    this.setState({
+      savedStream : this.canvasOutput.captureStream(vidLength*fps),
+    })
     if (delta > interval) {
       requestAnimationFrame(this.processVideo);
       this.setState({
@@ -408,67 +304,18 @@ class App extends Component {
     console.log('OpenCV.js is ready');
     this.startCamera();
   }
-  handleLHChange=(e)=>{
-  	this.setState({
-  		lh : parseInt(e.target.value)
-  	})
-  }
-  handleLSChange=(e)=>{
-  	this.setState({
-  		ls : parseInt(e.target.value)
-  	})
-  }
-  handleLVChange=(e)=>{
-  	this.setState({
-  		lv : parseInt(e.target.value)
-  	})
-  }
-  
-  handleHHChange=(e)=>{
-    this.setState({
-      hh : parseInt(e.target.value)
-    })
-  }
-  handleHSChange=(e)=>{
-    this.setState({
-      hs : parseInt(e.target.value)
-    })
-  }
-  handleHVChange=(e)=>{
-    this.setState({
-      hv : parseInt(e.target.value)
-    })
-  }
-  handleUpsideDown=()=>{
-    this.setState({
-      upsideDownMode : !this.state.upsideDownMode
-    })
-  }
+
   render() {
-  	
     return (
-      <div className="App">
-        <button  onClick={this.startCamera}>Start Video</button> 
-        <button  onClick={this.stopCamera}>Stop Video</button>      
-        <button  onClick={this.captureBackground}>Set Background</button>      
-        <button  onClick={this.handleUpsideDown}>Upside down mode</button>      
-
-         <div id="container">
+      <div className="App" style={{position: "relative"}}>
+        
+        <div id="lookHere"></div>
+        <div id="container">
             <h3>Fix your posture</h3>
-            <div className="sliders">
-
-              <input type="range" min={0} max={255} value={this.state.lh} onChange={this.handleLHChange}/>
-             	<input type="range" min={0} max={255} value={this.state.ls} onChange={this.handleLSChange}/>
-             	<input type="range" min={0} max={255} value={this.state.lv} onChange={this.handleLVChange}/>
-              <input type="range" min={0} max={255} value={this.state.hh} onChange={this.handleHHChange}/>
-              <input type="range" min={0} max={255} value={this.state.hs} onChange={this.handleHSChange}/>
-              <input type="range" min={0} max={255} value={this.state.hv} onChange={this.handleHVChange}/>
-            </div>
-            <p>{this.state.backgroundTimer}</p>
             <video className="invisible" ref={ref => this.video = ref}></video>
             <canvas ref={ref => this.canvasOutput = ref}  className="center-block" id="canvasOutput" width={320} height={240}></canvas>
-          </div>
         </div>
+      </div>
     );
   }
 }
